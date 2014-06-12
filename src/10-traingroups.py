23baf5df81be1998d394d72c2a3a5d778763d518
Esther Camilo <esthercamilo@gmail.com>
2014-06-12 19:13:49 -0300

    Threads para cálculos J48, geração de modelos e aplicação desses modelos no conjunto não-balanceado

--- src/10-traingroups.py
+++ src/10-traingroups.py
@@ -39,13 +39,25 @@ for elem in ftrain:
 size = min(len(listTrainNeg),len(listTrainPos))
 		
 def setGroups(indexStart,indexEnd,groupName):
-	indexStart = indexStart-2
-	indexEnd = indexEnd-1
+	thisHeader = ','.join(header[indexStart+2:indexEnd+2])+',class\n'
+	fumbalanced=open('weka/'+groupName+'/csv/exp/umbalanced.csv','w')
+	#fill umbalanced
+	fumbalanced.write(thisHeader)
+	for a in range(len(listTrainNeg)):
+		subUmb = ','.join([x for x in listTrainNeg[a][indexStart:indexEnd]])
+		fumbalanced.write(subUmb+',AGG\n')
+	for a in range(len(listTrainPos)):
+		subUmb = ','.join([x for x in listTrainPos[a][indexStart:indexEnd]])
+		fumbalanced.write(subUmb+',ALL\n')
+	fumbalanced.close()
+	
+	
+	#fill balanced
 	for i in range(1,101):
 		file_exp = open('weka/'+groupName+'/csv/exp/'+str(i)+'.csv','w')
 		file_ran = open('weka/'+groupName+'/csv/random/'+str(i)+'.csv','w')
-		file_exp.write(','.join(header[indexStart+2:indexEnd+2])+',class\n')
-		file_ran.write(','.join(header[indexStart+2:indexEnd+2])+',class\n')
+		file_exp.write(thisHeader)
+		file_ran.write(thisHeader)
 		rm.shuffle(listTrainNeg)
 		rm.shuffle(listTrainPos)
 		for j in range(size):
@@ -58,25 +70,28 @@ def setGroups(indexStart,indexEnd,groupName):
 			file_exp.write(','.join(subPos)+',ALL\n')
 			rm.shuffle(classes)
 			file_ran.write(','.join(subPos)+','+classes[0]+'\n')
-			
+		file_exp.close()
+		file_ran.close()
+		
+		
 #DEGREE
-i_start = header.index('degInt_min')
-i_end = header.index('degmet_max')
+i_start = header.index('degInt_min')-2
+i_end = header.index('degmet_max')-1
 setGroups(i_start,i_end,'deg')
 
 #BETWEENESS
-i_start = header.index('betInt_min')
-i_end = header.index('betmet_max')
+i_start = header.index('betInt_min')-2
+i_end = header.index('betmet_max')-1
 setGroups(i_start,i_end,'bet')
 
 #NEIGHBORHOOD
-i_start = header.index('cnInt')
-i_end = header.index('jcmet')
+i_start = header.index('cnInt')-2
+i_end = header.index('jcmet')-1
 setGroups(i_start,i_end,'neigh')
 
 #COMPLETE
-i_start = header.index('degInt_min')
-i_end = header.index('jcmet')
+i_start = header.index('degInt_min')-2
+i_end = header.index('jcmet')-1
 setGroups(i_start,i_end,'complete')
 
 
