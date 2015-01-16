4cd1c00a1389e21c6a2da3e71d97435aa1dc57f2
Esther Camilo <esthercamilo@gmail.com>
2015-01-16 12:50:54 -0200

    update babu

--- src/09b-training_umb_Babu.py
+++ src/09b-training_umb_Babu.py
@@ -1,4 +1,4 @@
-#################################
+# ################################
 #AUTHOR: ESTHER CAMILO          #
 #e-mail: esthercamilo@gmail.com #
 #################################
@@ -7,49 +7,55 @@ fcfg = open('config.txt')
 folder = fcfg.readline().rstrip('\n')
 
 # open files
-fcent = open(folder+'files/cent_all.tab')
+fcent = open(folder + 'files/cent_all.tab')
 hcent = fcent.readline().split()
-fneig = open(folder+'files/neigh_all.tab')
+fneig = open(folder + 'files/neigh_all.tab')
 hneig = fneig.readline().split()
-fshor = open(folder+'files/spathsall.tab')
+fshor = open(folder + 'files/spathsall.tab')
 hshor = fshor.readline().split()
-fbabu = open(folder+'data/s-score-babu.tab')
+fbabu = open(folder + 'data/s-score-babu.tab')
 hbabu = fbabu.readline()
 
 #read Babu file
-dicbabu={}    #numeric class
-dicnombabu={} #nominal class
+dicbabu = {}  #numeric class
+dicnombabu = {}  #nominal class
 for line in fbabu:
-    d = line.split()
-    float_d = float(d[2].replace(',', '.'))
-    classe = "AGG"
-    if float_d>0:
-        classe = "ALL"
-    dicbabu[(d[0],d[1])]=d[2]
-    dicnombabu[(d[0],d[1])]=classe
-    dicbabu[(d[1],d[0])]=d[2]
-    dicnombabu[(d[1],d[0])]=classe
+	d = line.split()
+	float_d = float(d[2].replace(',', '.'))
+	classe = "AGG"
+	if float_d > 0:
+		classe = "ALL"
+	dicbabu[(d[0], d[1])] = d[2]
+	dicnombabu[(d[0], d[1])] = classe
+	dicbabu[(d[1], d[0])] = d[2]
+	dicnombabu[(d[1], d[0])] = classe
 
-
-diccent={}
+diccent = {}
 for l in fcent:
-    d = l.split()
-    diccent[(d[0],d[1])]=d[2:]
+	d = l.split()
+	diccent[(d[0], d[1])] = d[2:]
 
-dicneigh={}
+dicneigh = {}
 for l in fneig:
-    d = l.split()
-    dicneigh[(d[0],d[1])]=d[2:]
+	d = l.split()
+	dicneigh[(d[0], d[1])] = d[2:]
 
-dicsp={}
+dicsp = {}
 for l in fshor:
-    d = l.split()
-    dicsp[(d[0],d[1])]=d[2:]
+	d = l.split()
+	dicsp[(d[0], d[1])] = d[2:]
 
-
-dic_complete={}
+dic_complete = {}
+output_nom = open(folder+'weka/Babu/umbalanced_nom.csv','w')
+output_num = open(folder+'weka/Babu/umbalanced_num.csv','w')
 for k in diccent.keys():
-    dic_complete[k] = diccent[k] + dicneigh[k] + dicsp[k]
+	try:
+		dic_complete[k] = diccent[k] + dicneigh[k] + dicsp[k] + dicbabu[k]
+		dic_completen[k] = diccent[k] + dicneigh[k] + dicsp[k] + dicnombabu[k]
+		output_nom.write(','.join(dic_completen[k])+'\n')
+		output_num.write(','.join(dic_complete[k])+'\n')
+	except:
+		pass
 
 
 
